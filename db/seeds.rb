78987eb9bae598da9cea132fd3c1323cf9195a56
Nick Powell <nickpowells@gmail.com>
2017-07-24 11:23:51 -0700

    add relationship post/responses

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,23 @@
+User.destroy_all
+Post.destroy_all
+Response.destroy_all
+
 1.times do |index|
   User.create!(email: "admin@gmail.com",
                         password: "password",
                         admin: true)
+end
+
+
+5.times do |index|
+  Post.create!(name: "Buzz Ramsey",
+                        title: "Too much fish in clackamas",
+                        content: "I just cant stand all these fish here")
+end
+
+
+10.times do |index|
+  Response.create!(name: "Owen",
+                        content: "No Kidding, Buzz",
+                        post_id: rand(1..5) )
 end
