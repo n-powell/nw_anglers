824449b7e81648438d2852a1e2070e545b695595
Nick Powell <nickpowells@gmail.com>
2017-07-24 10:13:31 -0700

    add landing page route, move later

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :responses
-  resources :posts
+  root to: "posts#landing"
+
+  resources :posts do
+    resources :responses
+  end
+
   devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
