9da76d8748cfea8a2ac8b7e46a8da60cfc94b1fe
Nick Powell <nickpowells@gmail.com>
2017-07-27 13:19:56 -0700

    add styling and turbolinks skips

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,16 @@ Response.destroy_all
                         admin: true)
 end
 
+1.times do |index|
+  User.create!(email: "user1@gmail.com",
+                        password: "password")
+end
+
+1.times do |index|
+  User.create!(email: "user2@gmail.com",
+                        password: "password")
+end
+
 
 5.times do |index|
   Post.create!(name: Faker::Name.name,
@@ -19,4 +29,11 @@ end
   Response.create!(name: Faker::Name.name,
                         content: Faker::MostInterestingManInTheWorld.quote,
                         post_id: rand(1..5) )
+end
+
+200.times do |index|
+  Trip.create!(address: ["Newport, Oregon", "Hagg Lake", "Astoria, Oregon", "Sandy river Oregon", "Willamette river", "Lake Oswego", "Wilson River Oregon", "Clackamas river oregon", "Fern Ridge Lake Oregon", "Triangle Lake Oregon", "Trillium Lake Oregon", "Frog Lake Oregon", "Clear Lake Oregon", "Ollala Resivoir Oregon", "Siletz River Oregon", "Yale Lake Washington", "Columbia River Oregon", "Cannon Beach Oregon"].sample,
+                        description: Faker::Lorem.paragraph(50, true, 4),
+                        trip_date: Faker::Date.between(700.days.ago, Date.today),
+                        user_id: rand(1..3) )
 end
