46a34b82b671ce36dcaa96a9a116953248ee8479
Nick Powell <nickpowells@gmail.com>
2017-10-23 15:33:08 -0700

    adjust seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,7 @@ end
 15.times do |index|
   Post.create!(name: Faker::Name.name,
                         title: Faker::Lorem.sentence(3, true, 4),
-                        content: Faker::Lorem.paragraph(2, false, 4))
+                        content: Faker::Lorem.paragraph(5, false, 4))
 end
 
 75.times do |index|
@@ -31,7 +31,7 @@ end
                         post_id: rand(1..5) )
 end
 
-200.times do |index|
+60.times do |index|
   Trip.create!(address: ["Newport, Oregon", "Hagg Lake", "Astoria, Oregon", "Sandy river Oregon", "Willamette river", "Lake Oswego", "Wilson River Oregon", "Clackamas river oregon", "Fern Ridge Lake Oregon", "Triangle Lake Oregon", "Trillium Lake Oregon", "Frog Lake Oregon", "Clear Lake Oregon", "Ollala Resivoir Oregon", "Siletz River Oregon", "Yale Lake Washington", "Columbia River Oregon", "Cannon Beach Oregon"].sample,
                         description: Faker::Lorem.paragraph(50, true, 4),
                         trip_date: Faker::Date.between(700.days.ago, Date.today),
